1a7e50632af114e24552780be7e6a7b280ecb4fe
Gregory <gregory_gr@outlook.com>
2025-02-21 11:29:18 -0300

    Add project files.

---
+++ WFConFin/Models/Cidade.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WFConFin.Models
+{
+    public class Cidade
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "O campo Nome deve ter entre 3 e 200 caracteres")]
+        public string Nome { get; set; }
+
+        // relacionamento entre as duas model, estado e cidade 
+        [Required(ErrorMessage = "O campo Estado é obrigatório")]
+        [StringLength(2, MinimumLength = 2 , ErrorMessage = "o campo Estado deve ter 2 caracteres")]
+        public string EstadoSigla { get; set; }
+
+        public Cidade()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        // relacionamento entity framework
+        public Estado Estado { get; set; }
+
+    }
+}
